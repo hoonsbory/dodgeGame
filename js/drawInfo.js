77d17407b6667ec788db8cc92db671a39c675de8
hoonsbory <53922603+hoonsbory@users.noreply.github.com>
2021-04-17 21:50:31 +0900

    .

--- js/drawInfo.js
+++ js/drawInfo.js
@@ -4,7 +4,7 @@ const drawInfo = (ctx,canvas,score) => {
     ctx.font = 'bold 16px Arial'
     ctx.textAlign = "center"
     ctx.fillText(`점수 : ${score}`, 100, 30)
-    ctx.fillText('made by 비트주세요', canvas.width / 2, canvas.height - 15)
+    ctx.fillText('made by 신재훈', canvas.width / 2, canvas.height - 15)
 }
 
 export default drawInfo
